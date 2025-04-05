2f0a7dc394f81fecd574630e2c1c10b09bf473b2
Taehoon Kee <taehoon.kee@protonmail.com>
2025-04-06 00:52:06 +0900

    꽤 오랜만에 커밋... 뭘 바꿨는지는 기억 안나는데, 어쨋든 시간이 흘러 작동 안하는 것 몇가지 고쳤던 것 같다... 가 아니고... call graph test를 여기서 했던 것 같다.

--- c1_exception/p01_try_except.py
+++ c1_exception/p01_try_except.py
@@ -1,10 +1,17 @@
+import sys
+import traceback
+
 try:
     print(1/0)
-except ZeroDivisionError:
-    print("ZeroDivisionError 예외가 발생했습니다.")
-except ArithmeticError:
-    print("ArithmeticError 예외가 발생했습니다.")
+#except ZeroDivisionError:
+#    print("ZeroDivisionError 예외가 발생했습니다.")
+#except ArithmeticError:
+#    print("ArithmeticError 예외가 발생했습니다.")
 except:
-    print("sdhfsakjdf")
-
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    print("예외 종류:", exc_type.__name__)
+    print(f'{exc_type=}')
+    print(f'{exc_value=}')
+    print(f'{exc_traceback=}')
+    traceback.print_tb(exc_traceback)
 print("Hello")
