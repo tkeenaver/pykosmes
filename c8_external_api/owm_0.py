3a2b664c2caea60a383465fed43c8b1eecc27819
Taehoon Kee <taehoon.kee@protonmail.com>
2025-06-25 07:25:06 +0900

    a little...

--- c8_external_api/owm_0.py
+++ c8_external_api/owm_0.py
@@ -1,5 +1,6 @@
 import requests
-API_KEY = '5f511454578c093f1a83e27026deef70'  # OpenWeatherMap에서 발급받은 API 키
+import pprint as pp
+API_KEY = '5f511454578c093f1a83e27026deef70'  # OpenWeatherMap에서 발급받은 API 키 (open-_pykosmes)
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 def get_weather(city):
     params = {
@@ -10,6 +11,6 @@ def get_weather(city):
     response = requests.get(BASE_URL, params=params)
     return response.json()
 if __name__ == '__main__':
-    city = 'Seoul'
+    city = 'London'
     weather_data = get_weather(city)
-    print(weather_data)
+    pp.pprint(weather_data)
---
+++ opencv_demo/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from opencv-demo!")
+
+
+if __name__ == "__main__":
+    main()
